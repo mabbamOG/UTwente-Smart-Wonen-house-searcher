f76d6755aa5f13bff35003de8e328dfea14876b7
M.A.B <mariobarbara@protonmail.ch>
2017-08-02 16:35:39 +0200

    added multiple searches, fixed bugs

--- scrape_smart_wonen.py
+++ scrape_smart_wonen.py
@@ -1,3 +1,4 @@
+### SMART WONEN SCRAPER
 import bs4
 import json
 import os
@@ -32,10 +33,16 @@ def print_all(houses):
 			s = f'{key:>20} {value:30}'
 			print(s)
 			f.write(s+'\n')
-	f.close()
 	
+## ONE ROOM
+#webpage = lambda i: f'https://www.smart-wonen.nl/en/page/{i}/?unonce=cf07b3c728&uformid=1927&s=uwpsfsearchtrg&taxo%5B0%5D%5Bname%5D=plaats&taxo%5B0%5D%5Bopt%5D&taxo%5B0%5D%5Bterm%5D=enschede&taxo%5B1%5D%5Bname%5D=type-woning&taxo%5B1%5D%5Bopt%5D&taxo%5B1%5D%5Bterm%5D=room&taxo%5B2%5D%5Bname%5D=oplevering-interieur&taxo%5B2%5D%5Bopt%5D&taxo%5B2%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B3%5D%5Bname%5D=aantal-kamer&taxo%5B3%5D%5Bopt%5D&taxo%5B3%5D%5Bterm%5D=1&cmf%5B0%5D%5Bmetakey%5D=prijs&cmf%5B0%5D%5Bcompare%5D=4&cmf%5B0%5D%5Bvalue%5D=uwpqsfcmfall&cmf%5B1%5D%5Bmetakey%5D=prijs&cmf%5B1%5D%5Bcompare%5D=6&cmf%5B1%5D%5Bvalue%5D=+500&skeyword'
+## TWO ROOMS
+#webpage = lambda i: f'https://www.smart-wonen.nl/en/page/{i}/?unonce=cf07b3c728&uformid=1927&s=uwpsfsearchtrg&taxo%5B0%5D%5Bname%5D=plaats&taxo%5B0%5D%5Bopt%5D&taxo%5B0%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B1%5D%5Bname%5D=type-woning&taxo%5B1%5D%5Bopt%5D&taxo%5B1%5D%5Bterm%5D=apartment&taxo%5B2%5D%5Bname%5D=oplevering-interieur&taxo%5B2%5D%5Bopt%5D&taxo%5B2%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B3%5D%5Bname%5D=aantal-kamer&taxo%5B3%5D%5Bopt%5D&taxo%5B3%5D%5Bterm%5D=2&cmf%5B0%5D%5Bmetakey%5D=prijs&cmf%5B0%5D%5Bcompare%5D=4&cmf%5B0%5D%5Bvalue%5D=uwpqsfcmfall&cmf%5B1%5D%5Bmetakey%5D=prijs&cmf%5B1%5D%5Bcompare%5D=6&cmf%5B1%5D%5Bvalue%5D=uwpqsfcmfall&skeyword'
+## THREE ROOMS
+#webpage = lambda i: f'https://www.smart-wonen.nl/en/page/{i}/?unonce=cf07b3c728&uformid=1927&s=uwpsfsearchtrg&taxo%5B0%5D%5Bname%5D=plaats&taxo%5B0%5D%5Bopt%5D=&taxo%5B0%5D%5Bterm%5D=enschede&taxo%5B1%5D%5Bname%5D=type-woning&taxo%5B1%5D%5Bopt%5D=&taxo%5B1%5D%5Bterm%5D=apartment&taxo%5B2%5D%5Bname%5D=oplevering-interieur&taxo%5B2%5D%5Bopt%5D=&taxo%5B2%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B3%5D%5Bname%5D=aantal-kamer&taxo%5B3%5D%5Bopt%5D=&taxo%5B3%5D%5Bterm%5D=3&cmf%5B0%5D%5Bmetakey%5D=prijs&cmf%5B0%5D%5Bcompare%5D=4&cmf%5B0%5D%5Bvalue%5D=uwpqsfcmfall&cmf%5B1%5D%5Bmetakey%5D=prijs&cmf%5B1%5D%5Bcompare%5D=6&cmf%5B1%5D%5Bvalue%5D=uwpqsfcmfall&skeyword='
+## APARTMENT ANY ROOMS
+webpage = lambda i: f'https://www.smart-wonen.nl/en/page/{i}/?unonce=57206afbec&uformid=1927&s=uwpsfsearchtrg&taxo%5B0%5D%5Bname%5D=plaats&taxo%5B0%5D%5Bopt%5D=&taxo%5B0%5D%5Bterm%5D=enschede&taxo%5B1%5D%5Bname%5D=type-woning&taxo%5B1%5D%5Bopt%5D=&taxo%5B1%5D%5Bterm%5D=apartment&taxo%5B2%5D%5Bname%5D=oplevering-interieur&taxo%5B2%5D%5Bopt%5D=&taxo%5B2%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B3%5D%5Bname%5D=aantal-kamer&taxo%5B3%5D%5Bopt%5D=&taxo%5B3%5D%5Bterm%5D=uwpqsftaxoall&cmf%5B0%5D%5Bmetakey%5D=prijs&cmf%5B0%5D%5Bcompare%5D=4&cmf%5B0%5D%5Bvalue%5D=uwpqsfcmfall&cmf%5B1%5D%5Bmetakey%5D=prijs&cmf%5B1%5D%5Bcompare%5D=6&cmf%5B1%5D%5Bvalue%5D=uwpqsfcmfall&skeyword='
 
-webpage = lambda i: f'https://www.smart-wonen.nl/en/page/{i}/?unonce=cf07b3c728&uformid=1927&s=uwpsfsearchtrg&taxo%5B0%5D%5Bname%5D=plaats&taxo%5B0%5D%5Bopt%5D&taxo%5B0%5D%5Bterm%5D=enschede&taxo%5B1%5D%5Bname%5D=type-woning&taxo%5B1%5D%5Bopt%5D&taxo%5B1%5D%5Bterm%5D=room&taxo%5B2%5D%5Bname%5D=oplevering-interieur&taxo%5B2%5D%5Bopt%5D&taxo%5B2%5D%5Bterm%5D=uwpqsftaxoall&taxo%5B3%5D%5Bname%5D=aantal-kamer&taxo%5B3%5D%5Bopt%5D&taxo%5B3%5D%5Bterm%5D=1&cmf%5B0%5D%5Bmetakey%5D=prijs&cmf%5B0%5D%5Bcompare%5D=4&cmf%5B0%5D%5Bvalue%5D=uwpqsfcmfall&cmf%5B1%5D%5Bmetakey%5D=prijs&cmf%5B1%5D%5Bcompare%5D=6&cmf%5B1%5D%5Bvalue%5D=+500&skeyword'
 houses = []
 ############### DOWNLOAD ALL ENTRIES
 if os.path.isfile('entries.json'):
@@ -68,9 +75,15 @@ else:
 	for i,house in enumerate(houses,1):
 		print(f'getting house {i} out of {len(houses)}..')
 		soup = bs4.BeautifulSoup(urllib.request.urlopen(house['url']), 'html.parser')
+		## get # of rooms
+		for tr in soup.find('table'):
+			key, value = [td.text for td in tr]
+			if key == 'Number of rooms:':
+				house[key] = value
+		## get address from map
 		map_link = soup.find('div', class_='googlemaps').iframe['src']
 		address = url_to_address(map_link)
-		print(f'got address {address}..')
+		#print(f'got address {address}..')
 		house['address'] = url_to_address(map_link)
 	with open('address_entries.json', 'w') as f:
 		f.write(json.dumps(houses, indent=2))
@@ -84,7 +97,12 @@ else:
 	ut_distance = lambda x: distance(UTWENTE, x)
 	for i,house in enumerate(houses,1):
 		print(f'calculating distance {i} out of {len(houses)}')
-		real_address = geo(house['address'], language='da')
+		for i in range(5):
+			try:
+				real_address = geo(house['address'], language='da')
+				break
+			except:
+				print('...retry')
 		house['real address'] = real_address.address
 		house['distance (km)'] = ut_distance(real_address)
 	with open('distance_entries.json', 'w') as f:
